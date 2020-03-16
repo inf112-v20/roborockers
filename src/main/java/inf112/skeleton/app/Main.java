1b071bb27d5242af12ad40f5a1dcd7bfbab444c0
pkl006 <pkl006@uib.no>
2020-03-16 14:33:40 +0100

    Fant ut av vi skulle endre på programkortene, her er de uten selected

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,6 +3,8 @@ package inf112.skeleton.app;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import java.awt.*;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +14,6 @@ public class Main {
         cfg.height = 900;
         cfg.resizable = false;
 
-        new LwjglApplication(new GameGraphics( ), cfg);
+        new LwjglApplication(new RallyGame( ), cfg);
     }
 }
